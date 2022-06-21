cb45ee6540faa70bbada78eaf27ecd97d2df7d7b
elyes ghidaoui <1403elghidaoui@student.epfc.eu>
2022-06-21 20:48:00 +0200

    final version 1

--- src/main/java/eu/epfc/movie/controllers/MovieController.java
+++ src/main/java/eu/epfc/movie/controllers/MovieController.java
@@ -3,12 +3,10 @@ package eu.epfc.movie.controllers;
 import eu.epfc.movie.entities.Movie;
 import eu.epfc.movie.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/eu/epfc/movie/repositories/MovieRepository.java
+++ src/main/java/eu/epfc/movie/repositories/MovieRepository.java
@@ -4,5 +4,4 @@ import eu.epfc.movie.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie,Long> {
-
 }
